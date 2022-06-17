6f8bf253dd2d3ae20ff3ac47de52c72f18fc0962
LucasAlv3s <lucasbiel40@gmail.com>
2022-06-16 23:21:02 -0300

    décimo commit

--- stack.py
+++ stack.py
@@ -1,3 +1,19 @@
+'''
+Example Basic
+'''
+from typing import List
+
+stack: List[str] = []
+
+stack.append('A')
+stack.append('B')
+stack.append('C')
+
+print(stack)
+
+"""
+Example POP
+"""
 from typing import List
 
 stack: List[str] = []
@@ -11,3 +27,27 @@ top_item = stack.pop() # B
 top_item = stack.pop() # A
 
 print(stack, top_item)
+
+"""
+Example Iterations and copy
+"""
+from typing import List
+from copy import deepcopy
+
+stack: List[str] = []
+
+stack.append('A')
+stack.append('B')
+stack.append('C')
+
+print('FOR: ')
+for item in stack[::-1]:
+    print(item)
+
+# copy
+stack_copy = deepcopy(stack)
+
+print('\nWHILE: ')
+while stack_copy:
+    item = stack_copy.pop()
+    print(item)
