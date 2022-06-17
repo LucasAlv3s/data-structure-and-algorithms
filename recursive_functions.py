1b5421413dce1a3006442070b4ea07df0427351b
LucasAlv3s <lucasbiel40@gmail.com>
2022-06-16 22:49:47 -0300

    sexto commit

--- recursive_functions.py
+++ recursive_functions.py
@@ -1,5 +1,8 @@
-# import sys
-# sys.setrecursionlimit(5000)
+# Example One:
+
+'''
+import sys
+sys.setrecursionlimit(5000)
 
 def factorial(num: int) -> int:
     # base case
@@ -11,3 +14,44 @@ def factorial(num: int) -> int:
 if __name__ == '__main__':
     factorial5 = factorial(5)
 print(factorial5)
+'''
+
+# Example Two:
+
+from typing import List
+from collections import namedtuple
+
+Cashier = namedtuple('Cashier', 'find_key')
+
+def find_key(cashier: List[Cashier], index: int = 0) -> Cashier:
+    # base case
+    if len(boxes) <= index:
+        return Cashier(False)
+    
+    cashier = boxes[index]
+
+    # base case
+    if cashier.find_key:
+        return Cashier
+
+    # recursive case    
+    index += 1
+    return find_key(boxes, index)
+
+if __name__ == '__main__':
+    boxes: List[Cashier] = [
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(True), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+        Cashier(False), Cashier(False), Cashier(False),
+    ]
+
+print(find_key(boxes))
